323e085aee573e4f6e76b6f2c394d31623583165
stewa342 <stewa342@purdue.edu>
2022-02-17 12:56:10 -0500

    added dependencies, LoginScreen, initial Routing

---
+++ frontend-react/src/Components/LoginScreen.js
@@ -0,0 +1,82 @@
+import { useState, useEffect } from 'react';
+import {TextField, Typography, Box, Paper, Button, Snackbar, SnackbarContent} from '@material-ui/core';
+import { Link, useNavigate } from 'react-router-dom';
+
+
+export default function LoginScreen() {
+  // clears history to prevent users from backing up to a restricted page
+  const nav = useNavigate();
+  useEffect(() => { nav('/login', { replace: true }); }, []);
+
+  // boolean for displaying missing-field error
+  const [alertBool, setAlertBool] = useState(false);
+
+  // tracks input fields
+  const [unameEmail, setUnameEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  // function to handle pressing the 'log in' button
+  function handleLogin() {
+    if (!unameEmail || !pwd) {
+      setAlertBool(true);
+      return;
+    }
+
+    const url = ""; // TODO: change to actual endpoint url
+
+    // connect to server enpoint
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        // TODO: authenticate
+      });
+  };
+
+  return (
+    <>
+    {/* for alert */}
+    <Snackbar
+    anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
+    open={alertBool}
+    onClose={e => setAlertBool(false)}
+    autoHideDuration={5000}>
+      <SnackbarContent style={{backgroundColor: "#D32F2F"}} message="Please fill in both fields."/>
+    </Snackbar>
+
+    {/* actual login screen */}
+    <div style={{
+      display: "flex",
+      backgroundColor: "#f5f5f5",
+      position: "absolute",
+      top: 0, left: 0,
+      width: "100vw",
+      height: "100vh"
+      }}>
+      <Paper elevation={8} style={{
+        backgroundColor: "#f5f5f5",
+        margin: "auto",
+        padding: "20px"
+      }} id="login_cont">
+        <Box m={2}><TextField label="Email/Username"
+        variant="outlined" type="text"
+        required size="small"
+        onChange={e => setUnameEmail(e.target.value)}/></Box>
+        <Box m={2}><TextField label="Password"
+        variant="outlined" type="password"
+        required size="small"
+        onChange={e => setPwd(e.target.value)}
+        /></Box>
+        <Box m={2} textAlign='center'>
+          <Button variant="outlined" color="primary" size="medium"
+          onClick={handleLogin}>Log In</Button>
+        </Box>    
+        <Box m={2} textAlign='center'>
+          <Typography>
+            <Link to="/signup">Need an account? Sign Up</Link>
+          </Typography>
+        </Box>  
+      </Paper>
+    </div>
+    </>
+  );
+}

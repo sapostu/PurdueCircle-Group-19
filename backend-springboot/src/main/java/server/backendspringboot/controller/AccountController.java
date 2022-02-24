46669ce1a7b07e9e1b0d2373f381dc38cc52feec
Cole Paradowski <cparadow@purdue.edu>
2022-02-23 21:09:53 -0500

    Additional functionality for the Account class, including search by ID and username

--- backend-springboot/src/main/java/server/backendspringboot/controller/AccountController.java
+++ backend-springboot/src/main/java/server/backendspringboot/controller/AccountController.java
@@ -1,14 +1,12 @@
 package server.backendspringboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import server.backendspringboot.model.Account;
 import server.backendspringboot.repository.AccountRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 
 @RestController
@@ -24,6 +22,34 @@ public class AccountController {
 
         
         return accountRepository.save(_account);
+    }
+
+    @GetMapping("/allAccounts")
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    @GetMapping("/getAccountbyID/{account_id}")
+    public Account getAccountById(@PathVariable int account_id) {
+        return accountRepository.findById(account_id).orElseThrow(() ->
+                new IllegalStateException("No account with id " + account_id));
+    }
+
+    @GetMapping("/getByUsername/{username}")
+    public Account getAccountByUsername(@PathVariable String username) {
+        return accountRepository.getAccountByUsername(username);
+    }
+
+    @DeleteMapping("/deleteById/{account_id}")
+    public void deleteById(@PathVariable int account_id) {
+        accountRepository.deleteById(account_id);
+    }
+
+    @DeleteMapping("/deleteByUsername/{username}")
+    public Account deleteByUsername(@PathVariable String username) {
+        Account toDel = accountRepository.getAccountByUsername(username);
+        accountRepository.delete(toDel);
+        return toDel;
     }
     
 }
--- backend-springboot/src/main/java/server/backendspringboot/repository/AccountRepository.java
+++ backend-springboot/src/main/java/server/backendspringboot/repository/AccountRepository.java
@@ -10,6 +10,9 @@ public interface AccountRepository extends JpaRepository<Account, Integer>{
     // NOTE: this is the correct format but i don't know how to pass in args. will us springboot library to save. 
     // IF INCORRECT AND NOT USED, spring boot will not work. leave it commented unless it works for sure :)
 
+    @Query("SELECT s FROM Account s WHERE s.username =?1")
+    Account getAccountByUsername(String username);
+
     // @Query(
     //     value = "INSERT INTO account_info(username, email, crypt_password) VALUES (?1, ?2, ?3);",
     //     nativeQuery = true

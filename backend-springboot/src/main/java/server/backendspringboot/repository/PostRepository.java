8218ba2e8f66932b8999618f0cae7be26ffb7d37
simpsontg <simpso98@purdue.edu>
2022-03-30 15:18:40 -0400

    backend changes for post/controller/repository to support the topic page

--- backend-springboot/src/main/java/server/backendspringboot/repository/PostRepository.java
+++ backend-springboot/src/main/java/server/backendspringboot/repository/PostRepository.java
@@ -3,6 +3,7 @@ package server.backendspringboot.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import server.backendspringboot.model.Post;
+import server.backendspringboot.model.Tags;
 
 import java.util.List;
 
@@ -10,4 +11,10 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("SELECT s FROM Post s WHERE s.accountId =?1")
     List<Post> getPostByAuthor(long authorId);
+
+    @Query(value = "SELECT account.username as name, post.* FROM post INNER JOIN account ON post.account_id=account.account_id WHERE post.tag_id = ?1", nativeQuery = true)
+    List<Post> getPostByTag_id(long tagId);
+
+    @Query("SELECT s.tagId FROM Tags s WHERE s.tagName =?1")
+    Long getTagsByName(String name);
 }

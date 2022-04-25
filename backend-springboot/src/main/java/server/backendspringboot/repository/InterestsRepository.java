bbdbc9bce54195a6e761a7b9a9f7ded94ca2f050
simpsontg <simpso98@purdue.edu>
2022-04-25 00:47:33 -0400

    the interests you follow now appear on the following page; you can unfollow your interests from this page

--- backend-springboot/src/main/java/server/backendspringboot/repository/InterestsRepository.java
+++ backend-springboot/src/main/java/server/backendspringboot/repository/InterestsRepository.java
@@ -16,6 +16,9 @@ public interface InterestsRepository extends JpaRepository<Interests, Long> {
     @Query("SELECT s FROM Interests s WHERE s.account_id = ?1 AND s.tag_id = ?2")
     Interests getInterestsByAccount_idAndTag_id(int accountId, Long tagId);
 
+    @Query(value = "SELECT tags.tag_name as name, interests.* FROM interests INNER JOIN tags ON tags.tag_id=interests.tag_id WHERE interests.account_id = ?1", nativeQuery = true)
+    List<Interests> getTagNamesByAccount_id(int accountId);
+
     @Transactional
     @Modifying
     @Query("DELETE FROM Interests s WHERE s.account_id = ?1 AND s.tag_id = ?2")

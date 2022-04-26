fe4c205c7a37e740318f757dcff8dad713954842
stewa342 <stewa342@purdue.edu>
2022-04-26 18:37:00 -0400

    added to save-post backend functionality

--- backend-springboot/src/main/java/server/backendspringboot/repository/Posts_SavedRepository.java
+++ backend-springboot/src/main/java/server/backendspringboot/repository/Posts_SavedRepository.java
@@ -2,6 +2,7 @@ package server.backendspringboot.repository;
 
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -12,6 +13,13 @@ public interface Posts_SavedRepository extends JpaRepository<posts_saved, Long> 
 
     @Query(value = "SELECT * FROM posts_saved WHERE account_id =?1", nativeQuery = true)
     List<posts_saved> getPostsSavedByAccountId(Long account_id);
+
+    @Query(value = "SELECT * FROM posts_saved WHERE account_id = ?1 AND post_id = ?2", nativeQuery = true)
+    List<posts_saved> getDuplicates(Long account_id, Long post_id);
+
+    @Modifying
+    @Query(value = "DELETE FROM posts_saved WHERE account_id = ?1 AND post_id = ?2", nativeQuery = true)
+    void unsavePost(Long account_id, Long post_id);
 
     // @Transactional
     // @Modifying

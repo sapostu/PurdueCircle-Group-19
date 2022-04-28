d009ef648e1ee226d5ed2a009531038bb202fa4d
Cole Paradowski <cparadow@purdue.edu>
2022-04-28 18:18:35 -0400

    backend functionality for DM flags

--- backend-springboot/src/main/java/server/backendspringboot/controller/AccountController.java
+++ backend-springboot/src/main/java/server/backendspringboot/controller/AccountController.java
@@ -21,6 +21,7 @@ public class AccountController {
     @Autowired
     PostController pc;
 
+
     @PostMapping("/signup")
     public Account createAccount(@RequestBody Account _account) {
         //TODO: process POST request
@@ -155,6 +156,23 @@ public class AccountController {
 
         edit.setUsername(account.getUsername());
         return edit;
+    }
+
+    @Transactional
+    @GetMapping("/reqDM/{id}")
+    public Account editDMReq(@PathVariable String id) {
+        int real_id = Integer.parseInt(id);
+        Account account = accountRepository.findById(real_id).orElse(null);
+        if (account == null) {
+            return null;
+        }
+        if (account.getReq_following() == 0L) {
+            account.setReq_following(1L);
+        } else {
+            account.setReq_following(0L);
+        }
+        accountRepository.setReqDM(account.getAccount_id(), account.getReq_following()); // write to database
+        return account;
     }
 
 
